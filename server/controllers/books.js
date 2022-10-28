178f12f5c84c167a4518f1ae3e61ca7de1e762d2
JasonCMLo <jason.chun.man.lo@gmail.com>
2022-10-28 14:51:59 -0400

    Functionality to edit and remove books

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -44,21 +44,54 @@ export function processAddPage(req, res, next) {
 
 // GET the Book Details page in order to edit an existing Book
 export function displayEditPage(req, res, next) {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+  let bookID = req.params.id;
+
+  booksModel.findById(bookID, (err, book) => {
+    if (err) {
+      console.error(err);
+      res.end(err);
+    }
+    res.render("index", {
+      title: "Edit Book",
+      page: "books/edit",
+      book: book,
+    });
+  });
 }
 
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+  let bookID = req.body.id;
+
+  let editBook = new booksModel({
+    _id: bookID,
+    name: req.body.name,
+    author: req.body.author,
+    published: req.body.published,
+    description: req.body.description,
+    price: req.body.price,
+  });
+
+  booksModel.updateOne({ _id: bookID }, editBook, (err, editBook) => {
+    if (err) {
+      console.error(err);
+      res.end(err);
+    }
+
+    res.redirect("/books/list");
+  });
 }
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-  /*****************
-   * ADD CODE HERE *
-   *****************/
+  let bookID = req.params.id;
+
+  booksModel.deleteOne({ _id: bookID }, (err) => {
+    if (err) {
+      console.error(err);
+      res.end(err);
+    }
+
+    res.redirect("/books/list");
+  });
 }
